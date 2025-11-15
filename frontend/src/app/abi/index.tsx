c5a40faab1cca770e2d50139c53cc89295dc5056
Abidoyesimze <similoluwaeyitayoabidoye@gmail.com>
2025-11-15 17:03:28 +0100

    chore: add payroll and multisig abis

---
+++ frontend/src/app/abi/index.tsx
@@ -0,0 +1,12 @@
+import Multisig from "./Multisig.json";
+import Payroll from "./Payroll.json";
+
+export const MultisigContract = {
+    abi: Multisig,
+    address: "0xBe84f02eAD3968cE4330fA371b9A19B0819E3Bb8",
+}
+
+export const PayrollContract = {
+    abi: Payroll,
+    address: "0x45CFFa961b1DE99A54DfBD127239Ce2be774Ab84",
+}
